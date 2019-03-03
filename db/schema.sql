388c22e1a9850c85ecbddf5b14f46c193f9d1fb1
Ray Ray <raymond2129@gmail.com>
2019-03-03 14:21:41 -0700

    update all files

--- db/schema.sql
+++ db/schema.sql
@@ -6,9 +6,9 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
-	date TIMESTAMP,
+	createdAt TIMESTAMP NOT NULL,
 	PRIMARY KEY (id)
 );
