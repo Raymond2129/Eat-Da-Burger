89483c4f648ea4b0251c8e4150563fd953da71de
Ray Ray <raymond2129@gmail.com>
2019-03-03 09:33:07 -0700

    updated layouts.html

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         host: "localhost",
         user: "root",
-        password: "Kodiak2129!",
+        password: "",
         database: "burgers_db"
     });
 };
--- db/schema.sql
+++ db/schema.sql
@@ -2,15 +2,13 @@
 DROP DATABASE IF EXISTS burgers_db;
 -- Create the burgers database
 CREATE DATABASE burgers_db;
-
 USE burgers_db;
--- Create the burger table
-CREATE TABLE burgers 
+
+-- Create the table burgers.
+CREATE TABLE burgers
 (
     id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(255) NOT NULL,
+    burger_name VARCHAR(50) NOT NULL,
     devoured BOOLEAN DEFAULT false,
-    ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    dt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
