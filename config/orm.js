fd86c25c53c6a6f1bc7ac3dc8bf75d7431dacaa4
Ray Ray <raymond2129@gmail.com>
2019-03-03 10:45:23 -0700

    fixed orm

--- config/orm.js
+++ config/orm.js
@@ -1,79 +1,94 @@
-// Import MySQL connection.
-var connection = require("../config/connection.js");
+// Import the MySQL connection object
+var connection = require ('./connection.js');
 
-// Helper function for SQL syntax.
+// Helper function for generating MySQL syntax
 function printQuestionMarks(num) {
-  var arr = [];
+	var arr = [];
 
-  for (var i = 0; i < num; i++) {
-    arr.push("?");
-  }
+	for (var i = 0; i < num; i++) {
+		arr.push("?");
+	}
 
-  return arr.toString();
+	return arr.toString();
 }
 
-// Helper function for SQL syntax.
+// Helper function for generating My SQL syntax
 function objToSql(ob) {
-  var arr = [];
+	var arr = [];
 
-  for (var key in ob) {
-    if (Object.hasOwnProperty.call(ob, key)) {
-      arr.push(key + "=" + ob[key]);
-    }
-  }
+	for (var key in ob) {
+		arr.push(key + "=" + ob[key]);
+	}
 
-  return arr.toString();
+	return arr.toString();
 }
 
-// Object for all our SQL statement functions.
+// Create the ORM object to perform SQL queries
 var orm = {
-  all: function(tableInput, cb) {
-    var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-      cb(result);
-    });
-  },
-  create: function(table, cols, vals, cb) {
-    var queryString = "INSERT INTO " + table;
+	// Function that returns all table entries
+	selectAll: function(tableInput, cb) {
+		// Construct the query string that returns all rows from the target table
+		var queryString = "SELECT * FROM " + tableInput + ";";
 
-    queryString += " (";
-    queryString += cols.toString();
-    queryString += ") ";
-    queryString += "VALUES (";
-    queryString += printQuestionMarks(vals.length);
-    queryString += ") ";
+		// Perform the database query
+		connection.query(queryString, function(err, result) {
+			if (err) {
+				throw err;
+			}
 
-    console.log(queryString);
+			// Return results in callback
+			cb(result);
+		});
+	},
 
-    connection.query(queryString, vals, function(err, result) {
-      if (err) {
-        throw err;
-      }
-      cb(result);
-    });
-  },
-  // An example of objColVals would be {name: panther, sleepy: true}
-  update: function(table, objColVals, condition, cb) {
-    var queryString = "UPDATE " + table;
+	// Function that insert a single table entry
+	insertOne: function(table, cols, vals, cb) {
+		// Construct the query string that inserts a single row into the target table
+		var queryString = "INSERT INTO " + table;
 
-    queryString += " SET ";
-    queryString += objToSql(objColVals);
-    queryString += " WHERE ";
-    queryString += condition;
+		queryString += " (";
+		queryString += cols.toString();
+		queryString += ") ";
+		queryString += "VALUES (";
+		queryString += printQuestionMarks(vals.length);
+		queryString += ") ";
 
-    console.log(queryString);
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
+		// console.log(queryString);
 
-      cb(result);
-    });
-  }
+		// Perform the database query
+		connection.query(queryString, vals, function(err, result) {
+			if (err) {
+				throw err;
+			}
+
+			// Return results in callback
+			cb(result);
+		});
+	},
+
+	// Function that updates a single table entry
+	updateOne: function(table, objColVals, condition, cb) {
+		// Construct the query string that updates a single entry in the target table
+		var queryString = "UPDATE " + table;
+
+		queryString += " SET ";
+		queryString += objToSql(objColVals);
+		queryString += " WHERE ";
+		queryString += condition;
+
+		// console.log(queryString);
+
+		// Perform the database query
+		connection.query(queryString, function(err, result) {
+			if (err) {
+				throw err;
+			}
+
+			// Return results in callback
+			cb(result);
+		});
+	}
 };
 
-// Export the orm object for the model (cat.js).
+// Export the orm object for use in other modules
 module.exports = orm;
